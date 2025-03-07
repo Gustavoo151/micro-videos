073267f5abc6b6b5e9141302064bde6e0bd29642
gustavoo151 <josegustavooliveira121@gmail.com>
2025-03-07 09:11:00 +0000

    refactor: enhance Category entity with validation error handling and code formatting

--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -1,76 +1,81 @@
-import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
-import { CategoryValidatorFactory } from "./category.validator";
+import { EntityValidationErrors } from '../../shared/domain/validators/validation.error';
+import { Uuid } from '../../shared/domain/value-objects/uuid.vo';
+import { CategoryValidatorFactory } from './category.validator';
 
 export type CategoryConstructorProps = {
-    category_id?: Uuid;
-    name: string;
-    description?: string | null;
-    is_active?: boolean;
-    created_at?: Date;
+  category_id?: Uuid;
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+  created_at?: Date;
 };
 
-export type CategoryCreateCommand = { 
-    name: string;
-    description?: string | null;
-    is_active?: boolean;
+export type CategoryCreateCommand = {
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
 };
 
 export class Category {
-    category_id: Uuid;
-    name: string;
-    description: string | null;
-    is_active: boolean;
-    created_at: Date;
-    
-    constructor(props: CategoryConstructorProps) {
-        this.category_id = props.category_id ?? new Uuid();
-        this.name = props.name;
-        this.description = props.description ?? null;
-        this.is_active = props.is_active ?? true;
-        this.created_at = props.created_at ?? new Date();
-    }
+  category_id: Uuid;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
 
-    // (Esse create a um factory metodo)
-    static create(props: CategoryCreateCommand): Category {
-        const category = new Category(props);
-        Category.validate(category);
-        return category;
-    } 
+  constructor(props: CategoryConstructorProps) {
+    this.category_id = props.category_id ?? new Uuid();
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.is_active = props.is_active ?? true;
+    this.created_at = props.created_at ?? new Date();
+  }
 
-    update(props: Partial<CategoryConstructorProps>): Category {
-        return new Category({ ...this, ...props});
-    }
+  // (Esse create a um factory metodo)
+  static create(props: CategoryCreateCommand): Category {
+    const category = new Category(props);
+    Category.validate(category);
+    return category;
+  }
 
-    changeName(name: string): void {
-        this.name = name;
-        Category.validate(this);
-    }
+  update(props: Partial<CategoryConstructorProps>): Category {
+    return new Category({ ...this, ...props });
+  }
 
-    changeDescription(description: string): void {
-        this.description = description;
-        Category.validate(this);
-    }
+  changeName(name: string): void {
+    this.name = name;
+    Category.validate(this);
+  }
 
-    activate(){
-        this.is_active = true;
-    }
+  changeDescription(description: string): void {
+    this.description = description;
+    Category.validate(this);
+  }
 
-    deactivate(){
-        this.is_active = false;
-    }
+  activate() {
+    this.is_active = true;
+  }
 
-    static validate(entity: Category){
-        const validator = CategoryValidatorFactory.create();
-        return validator.validate(entity);
-    }
+  deactivate() {
+    this.is_active = false;
+  }
 
-    toJSON(){
-        return {
-            category_id: this.category_id.id,
-            name: this.name,
-            description: this.description,
-            is_active: this.is_active,
-            created_at: this.created_at
-        };
+  static validate(entity: Category) {
+    const validator = CategoryValidatorFactory.create();
+    const isValid = validator.validate(entity);
+
+    if (!isValid) {
+      throw new EntityValidationErrors(validator.errors);
     }
+  }
+
+  toJSON() {
+    return {
+      category_id: this.category_id.id,
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      created_at: this.created_at,
+    };
+  }
 }
