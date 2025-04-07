36681397a08ff9c7251f2f67dab604dd621cb4fa
gustavoo151 <josegustavooliveira121@gmail.com>
2025-04-07 00:46:22 -0300

    refactor: standardize formatting and improve type definitions in IRepository interface

--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -1,14 +1,14 @@
-import { Entity } from "../entity";
-import { ValueObject } from "../value-object";
+import { Entity } from '../entity';
+import { ValueObject } from '../value-object';
 
 export interface IRepository<E extends Entity, Entity_Id extends ValueObject> {
-    insert(entity: E): Promise<void>;
-    bulkInsert(entities: E[]): Promise<void>;
-    update(entity: E): Promise<void>;
-    delete(entity_id: E): Promise<void>;
+  insert(entity: E): Promise<void>;
+  bulkInsert(entities: E[]): Promise<void>;
+  update(entity: E): Promise<void>;
+  delete(entity_id: Entity_Id): Promise<void>;
 
-    findById(entity_id: Entity_Id): Promise<E>;
-    findAll(): Promise<E[]>;
+  findById(entity_id: Entity_Id): Promise<E | null>;
+  findAll(): Promise<E[]>;
 
-    getEntity(): new (...args: any[]) => E;
+  getEntity(): new (...args: any[]) => E;
 }
