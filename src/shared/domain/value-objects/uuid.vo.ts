1e528ba2c76e80fa1064af9612f59276a8b63796
gustavoo151 <josegustavooliveira121@gmail.com>
2025-06-30 02:50:53 -0300

    style: standardize formatting and improve readability in Uuid and InvalidUuidError classes

--- src/shared/domain/value-objects/uuid.vo.ts
+++ src/shared/domain/value-objects/uuid.vo.ts
@@ -1,26 +1,30 @@
-import { ValueObject } from "../value-object";
-import {v4 as uuidv4, validate as uuidValidate} from "uuid";
+import { ValueObject } from '../value-object';
+import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
 
 export class Uuid extends ValueObject {
-    readonly id: string; // Esse readOnly ser para deixar esse campo para apenas "leitura"
-    
-    constructor(id?: string){
-        super();
-        this.id = id || uuidv4();
-        this.validate();
-    }
+  readonly id: string; // Esse readOnly ser para deixar esse campo para apenas "leitura"
 
-    private validate(){
-        const isValid = uuidValidate(this.id);
-        if(!isValid) {
-            throw new InvalidUuidError();
-        }
+  constructor(id?: string) {
+    super();
+    this.id = id || uuidv4();
+    this.validate();
+  }
+
+  private validate() {
+    const isValid = uuidValidate(this.id);
+    if (!isValid) {
+      throw new InvalidUuidError();
     }
+  }
+
+  toString() {
+    return this.id;
+  }
 }
 
 export class InvalidUuidError extends Error {
-    constructor(message?: string){
-        super(message || 'ID must be a valid UUID');
-        this.name = "InvalidUuidError";
-    }
+  constructor(message?: string) {
+    super(message || 'ID must be a valid UUID');
+    this.name = 'InvalidUuidError';
+  }
 }
